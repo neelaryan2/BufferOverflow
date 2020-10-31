0f88d5cb52de108e9fc6c848f46d80dfe55492ae
Neel <neelaryan2@gmail.com>
2020-10-31 11:23:48 +0530

    updated ret2syscall

--- ret2syscall/ret2syscall.c
+++ ret2syscall/ret2syscall.c
@@ -1,27 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#define BUFSIZE 100
-
 void vuln() {
-	printf("Get access to the shell!\n");
-	char input[BUFSIZE];
-	fgets(input, 360, stdin);
+	char input[32];
+	read(0, input, 150);
 }
 
 int main(int argc, char **argv){
-	setvbuf(stdout, NULL, _IONBF, 0);
-	
-	// Set the gid to the effective gid
-	// this prevents /bin/sh from dropping the privileges
-	gid_t gid = getegid();
-	setresgid(gid, gid, gid);
+
+	puts("Get access to the shell!");
 	
 	vuln();
-	printf("You failed! Better luck next time.\n");
+
+	puts("You failed! Better luck next time.");
 
 	return 0;
 }
