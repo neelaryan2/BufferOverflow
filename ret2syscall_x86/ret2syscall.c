34e40e0bcb1d2a5c502cefc9ed4043bfddbda72c
Neel <neelaryan2@gmail.com>
2020-10-31 12:23:33 +0530

    added ret2syscall x86

---
+++ ret2syscall_x86/ret2syscall.c
@@ -0,0 +1,15 @@
+void vuln() {
+	char input[32];
+	read(0, input, 150);
+}
+
+int main(int argc, char **argv){
+
+	puts("Get access to the shell!");
+	
+	vuln();
+
+	puts("You failed! Better luck next time.");
+
+	return 0;
+}
